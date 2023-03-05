ddac72cf1f3a6160c05bcd877ec346888cef49db
Nico Miguelino <nicomiguelino2014@gmail.com>
2023-03-05 08:46:13 +0800

    Create autocommands.

--- init.lua
+++ init.lua
@@ -20,4 +20,24 @@ function set_global_options()
     vim.opt.cursorline = true
 end
 
+function set_autocommands()
+    max_line_length = {
+        python = '79',
+        javascript = '79',
+        ruby = '79',
+        html = '100',
+        htmldjango = '100',
+    }
+
+    for language, length in pairs(max_line_length) do
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = language,
+            callback = function(args)
+                vim.opt_local.colorcolumn = length
+            end
+        })
+    end
+end
+
 set_global_options()
+set_autocommands()
